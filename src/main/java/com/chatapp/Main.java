8752ad9d83a17daface1c6782a8704302dc047e2
Splotchy2 <st10507345@rcconnect.edu.za>
2026-04-06 14:09:17 +0200

    Added files to GitHub
    Initial commit of files

---
+++ src/main/java/com/chatapp/Main.java
@@ -0,0 +1,8 @@
+package com.chatapp;
+
+
+public class Main {
+    static void main() {
+
+    }
+}
---
+++ src/test/java/com/chatapp/MainTest.java
@@ -0,0 +1,13 @@
+package com.chatapp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+        assertEquals(1, 2, "They are not equal!");
+    }
+}
